b1a62ffbce8f69ffed3af8b230dcd98fcb76bb4e
chiench <quangminhb53@gmail.com>
2023-02-20 10:57:11 +0700

    install Jestream and make Auth

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,13 @@ Route::get('/',HomeComponent::class);
 Route::get('/shop',ShopComponent::class);
 Route::get('/cart',CartComponent::class);
 Route::get('/checkout',CheckoutComponent::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
